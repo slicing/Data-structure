6568512ea9c27121d7d8ec881ddfd2ef009878cd
slicing <slow_slicing@qq.com>
2018-11-13 16:42:53 +0800

    red black tree end

--- Datastructure/src/Slow/slicing/red_black_tree/RBTree.java
+++ Datastructure/src/Slow/slicing/red_black_tree/RBTree.java
@@ -1,5 +1,14 @@
 package Slow.slicing.red_black_tree;
 
+import com.sun.deploy.util.BlackList;
+import com.sun.org.apache.xpath.internal.operations.Or;
+import org.jcp.xml.dsig.internal.dom.ApacheCanonicalizer;
+import sun.font.ExtendedTextLabel;
+import sun.security.krb5.internal.APRep;
+import sun.text.normalizer.Replaceable;
+
+import javax.print.attribute.standard.MediaSize;
+import javax.swing.*;
 import java.util.PrimitiveIterator;
 
 public class RBTree<T extends Comparable<T>>{
@@ -51,7 +60,7 @@ public class RBTree<T extends Comparable<T>>{
 		if (node!=null)
 			node.color = BLACK;
 	}
-	private void serRed(RBNode<T> node){
+	private void setRed(RBNode<T> node){
 		if (node!=null)
 			node.color = RED;
 	}
@@ -63,6 +72,124 @@ public class RBTree<T extends Comparable<T>>{
 		if (node!= null)
 			node.color = color;
 	}
+	//前序遍历红黑树
+	private void preOrder(RBNode<T> tree){
+		if (tree != null){
+			System.out.print(tree.key + " ");
+			preOrder(tree.left);
+			preOrder(tree.right);
+		}
+	}
+	public void prerOder(){
+		preOrder(mRoot);
+	}
+
+	//中序
+	private void inOrder(RBNode<T> tree){
+		if (tree!= null){
+			inOrder(tree.left);
+			System.out.print(tree.key + " ");
+			inOrder(tree.right);
+		}
+	}
+	public void inOrder(){
+		inOrder(mRoot);
+	}
+	//后序
+	private void postOrder(RBNode<T> tree){
+		if (tree != null){
+			postOrder(tree.left);
+			postOrder(tree.right);
+			System.out.print(tree.key + " ");
+		}
+	}
+	public void postOrder(){
+		postOrder(mRoot);
+	}
+	/*递归查找，红黑树中键值为key的结点*/
+	private RBNode<T> search(RBNode<T> x,T key){
+		if (x == null)
+			return x;
+		int cmp = key.compareTo(x.key);
+		if (cmp < 0)
+			return search(x.left,key);
+		else if (cmp > 0)
+			return search(x.right,key);
+		else
+			return x;
+	}
+	public RBNode<T> search(T key){
+		return search(mRoot,key);
+	}
+	/*非递归实现查找*/
+	private RBNode<T> iterativeSearch(RBNode<T> x,T key){
+		while (x != null){
+			int cmp = key.compareTo(x.key);
+			if (cmp < 0)
+				x = x.left;
+			else if (cmp>0)
+				x = x.right;
+			else
+				return x;
+		}
+		return x;
+	}
+	public RBNode<T> iterativeSearch(T key){
+		return iterativeSearch(mRoot,key);
+	}
+
+	/*查找最小结点，返回tree为根结点的红黑树的最小结点*/
+	private RBNode<T> minimum(RBNode<T> tree){
+		if (tree == null)
+			return null;
+		while (tree.left != null)
+			tree = tree.left;
+		return tree;
+	}
+	public T minimum(){
+		RBNode<T> p = minimum(mRoot);
+		if (p != null)
+			return p.key;
+		return null;
+	}
+
+	/*查找最大节点*/
+	private RBNode<T> maximum(RBNode<T> tree){
+		if (tree == null)
+			return null;
+		while (tree.right != null)
+			tree = tree.right;
+		return tree;
+	}
+	public T maximum(){
+		RBNode<T> p = maximum(mRoot);
+		if (p != null)
+			return p.key;
+		return null;
+	}
+	/*找结点的后继结点*/
+	public RBNode<T> successor(RBNode<T> x){
+		if (x.right != null)
+			return minimum(x.right);
+		RBNode<T> y = x.parent;
+		while ((y!=null) && (x==y.right)){
+			x = y;
+			y = y.parent;
+		}
+		return y;
+	}
+	/*找该节点的前驱结点*/
+	public RBNode<T> predecessor(RBNode<T> x){
+		if (x.left != null)
+			return maximum(x.left);
+		RBNode<T> y = x.parent;
+		while ((y!=null) && (x==y.left)){
+			x = y;
+			y = y.parent;
+		}
+		return y;
+	}
+	/*左旋*/
 	private void leftRotate(RBNode<T> x){
 		RBNode<T> y =x.right;
 		x.right = y.left;
@@ -97,6 +224,7 @@ public class RBTree<T extends Comparable<T>>{
 		x.right = y;
 		y.parent = x;
 	}
+	/*将结点插入红黑树中*/
 	private void insert(RBNode<T> node){
 		int cmp;
 		RBNode<T> y = null;
@@ -127,8 +255,48 @@ public class RBTree<T extends Comparable<T>>{
 		RBNode<T> parent,gparent;
 		while (((parent = parentOf(node)) != null) && isRed(parent)){
 			gparent = parentOf(parent);
-
+			if (parent == gparent.left)
+			{
+				RBNode<T> uncle = gparent.right;
+				if ((uncle != null) && isRed(uncle)){
+					setBlack(uncle);
+					setBlack(parent);
+					setRed(gparent);
+					node = gparent;
+					continue;
+				}
+				if (parent.right == node){
+					RBNode<T> tmp;
+					leftRotate(parent);
+					tmp = parent;
+					parent = node;
+					node = tmp;
+				}
+				setBlack(parent);
+				setRed(gparent);
+				rightRotate(gparent);
+			}else {
+				RBNode<T> uncle = gparent.left;
+				if ((uncle != null) && isRed(uncle)){
+					setBlack(uncle);
+					setBlack(parent);
+					setRed(gparent);
+					node = gparent;
+					continue;
+				}
+				if (parent.left == node){
+					RBNode<T >tmp;
+					rightRotate(parent);
+					tmp = parent;
+					parent = node;
+					node = tmp;
+				}
+				setBlack(parent);
+				setRed(gparent);
+				leftRotate(gparent);
+			}
 		}
+		setBlack(this.mRoot);
 	}
 
 	public void insert(T key){
@@ -137,4 +305,157 @@ public class RBTree<T extends Comparable<T>>{
 			insert(node);
 	}
 
+	/*红黑树删除修正*/
+	private void removeFixUp(RBNode<T> node,RBNode<T> parent){
+		RBNode<T> other;
+		while ((node == null ||isBlack(node)) && (node != this.mRoot)){
+			if (parent.left == node){
+				other = parent.right;
+				if (isRed(other)){
+					setBlack(other);
+					setRed(parent);
+					leftRotate(parent);
+					other = parent.right;
+				}
+				if ((other.left == null || isBlack(other.left)) && (other.right == null || isBlack(other.right))){
+					setRed(other);
+					node = parent;
+					parent = parentOf(node);
+				}
+				else {
+					if (other.right == null || isBlack(other.right)){
+						setBlack(other.left);
+						setRed(other);
+						rightRotate(other);
+						other = parent.right;
+					}
+					setColor(other,colorOf(parent));
+					setBlack(parent);
+					setBlack(other.right);
+					leftRotate(parent);
+					node = this.mRoot;
+					break;
+				}
+			}
+			else {
+				other = parent.left;
+				if (isRed(other)){
+					setBlack(other);
+					setRed(parent);
+					rightRotate(parent);
+					other = parent.left;
+				}
+				if ((other.left == null || isBlack(other.left)) && (other.right == null) || isBlack(other.right)){
+					setRed(other);
+					node = parent;
+					parent = parentOf(node);
+				}else {
+					if (other.left == null || isBlack(other.left)){
+						setBlack(other.right);
+						setRed(other);
+						leftRotate(other);
+						other = parent.left;
+					}
+					setColor(other,colorOf(parent));
+					setBlack(parent);
+					setBlack(other.left);
+					rightRotate(parent);
+					node = this.mRoot;
+					break;
+				}
+			}
+		}
+		if (node!=null)
+			setBlack(node);
+	}
+	/*删除结点，并返回被删除的结点*/
+	private void remove(RBNode<T> node){
+		RBNode<T> child,parent;
+		boolean color;
+		if ((node.left!= null) && (node.right != null)){
+			RBNode<T> replace = node;
+			replace = replace.right;
+			while (replace.left != null)
+				replace = replace.left;
+			if (parentOf(node)!= null){
+				if (parentOf(node).left == node)
+					parentOf(node).left = replace;
+				else
+					parentOf(node).right = replace;
+			}else
+				this.mRoot = replace;
+			child = replace.right;
+			parent = parentOf(replace);
+			color = colorOf(replace);
+			if (parent == node) {
+				parent = replace;
+			}else {
+				if (child!=null)
+					setParent(child,parent);
+				parent.left = child;
+				replace.right = node.right;
+				setParent(node.right,replace);
+			}
+			replace.parent = node.parent;
+			replace.color = node.color;
+			replace.left = node.left;
+			node.left.parent = replace;
+			if (color == BLACK)
+				removeFixUp(child,parent);
+			node = null;
+			return;
+		}
+		if (node.left != null)
+			child = node.left;
+		else
+			child = node.right;
+		parent = node.parent;
+		color = node.color;
+		if (child != null)
+			child.parent = parent;
+		if (parent != null){
+			if (parent.left == node)
+				parent.left = child;
+			else
+				parent.right = child;
+		}else
+			this.mRoot = child;
+		if (color == BLACK)
+			removeFixUp(child,parent);
+		node = null;
+	}
+	public void remove(T key){
+		RBNode<T> node;
+		if ((node = search(mRoot,key)) != null)
+			remove(node);
+	}
+
+	/*销毁红黑树*/
+	private void destroy(RBNode<T> tree){
+		if (tree == null)
+			return;
+		if (tree.left !=null)
+			destroy(tree.left);
+		if (tree.right != null)
+			destroy(tree.right);
+		tree =null;
+	}
+	public void clear(){
+		destroy(mRoot);
+		mRoot = null;
+	}
+	private void print(RBNode<T> tree,T key,int direction){
+		if (tree != null){
+			if (direction == 0)
+				System.out.println(tree.key + "is root");
+			else
+				System.out.println(tree.key + (isRed(tree)?"R":"B") + " is " + key + "`s" + (direction == 1 ?"right":"left")+" child" );
+			print(tree.left,tree.key,-1);
+			print(tree.right,tree.key,1);
+		}
+	}
+	public void print(){
+		if (mRoot != null)
+			print(mRoot,mRoot.key,0);
+	}
 }
---
+++ Datastructure/src/Slow/slicing/red_black_tree/RBTreeAPP.java
@@ -0,0 +1,45 @@
+package Slow.slicing.red_black_tree;
+
+
+
+public class RBTreeAPP {
+	private static final int a[] = {10,40,30,60,90,70,20,50,80};
+	private static final boolean mDebugInsert = false;
+	private static final boolean mDebugDelete = false;
+
+	public static void main(String[] args) {
+		int i,ilen = a.length;
+		RBTree<Integer> tree = new RBTree<>();
+		System.out.println("==原始数据： ");
+		for (i = 0;i<ilen;i++)
+			System.out.print(a[i]);
+		System.out.println();
+		for (i = 0;i<ilen;i++){
+			tree.insert(a[i]);
+			if (mDebugInsert){
+				System.out.println(" == 添加节点" + a[i]);
+				System.out.println(" == 树得详细信息: ");
+				tree.print();
+				System.out.println();
+			}
+		}
+		System.out.println("== 前序遍历:");
+		tree.prerOder();
+		System.out.println("\n== 中序遍历:");
+		tree.postOrder();
+		System.out.println("\n== 后序遍历:");
+		tree.inOrder();
+		System.out.println();
+		if (mDebugDelete){
+			for (i=0;i<ilen;i++){
+				tree.remove(a[i]);
+				System.out.println("==删除结点： " + a[i]);
+				System.out.println("==树的详细信息: ");
+				tree.print();
+				System.out.println();
+			}
+			tree.clear();
+		}
+
+	}
+}
